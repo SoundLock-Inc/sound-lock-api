475d7bfd70d5d5cfe18c7de8590353c5fbe9ef5f
kkkivannn <ikiselev288@gmail.com>
2025-08-02 14:20:49 +0300

    Complete api service Auth and write tests for this

---
+++ migrations/000002_create_refresh_tokens_table.up.sql
@@ -0,0 +1,8 @@
+-- Таблица refresh токенов
+CREATE TABLE refresh_tokens (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    token TEXT NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
